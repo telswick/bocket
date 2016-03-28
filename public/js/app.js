daa3264f3ba91dd1e1011781b45945abf449d341
Traci Elswick <telswick@austin.rr.com>
2016-03-27 20:40:01 -0500

    Add HomeView and BookmarksListView

--- public/js/app.js
+++ public/js/app.js
@@ -30,4 +30,61 @@
 		model: SubbredditModel
 	});
 
+// Add Views for Home, Bookmarks, Tags	
+
+var HomeView = Backbone.View.extend({
+		el:'\
+			<div class="container">\
+				<div class="row">\
+					<div class="three columns"></div>\
+					<div class="six columns">\
+						<div class="row">\
+							<div class="twelve columns"></div>\
+						</div>\
+						<div class="row">\
+							<div class="twelve columns"></div>\
+						</div>\
+					</div>\
+					<div class="three columns" id="all-bookmarks"></div>\
+				</div>\
+			</div>\
+		',
+
+		render: function() {
+			var bookmarks = new BookmarksCollection();
+			bookmarks.fetch();
+			var bookmarksListView = new BookmarksListView({ 
+				collection: bookmarks
+			});
+			this.$el.find('#all-bookmarks').html(bookmarksListView.render().el);
+
+			return this;
+		}
+	});
+
+	var BookmarksListView = Backbone.View.extend({
+		el: '<ul></ul>',
+
+		template: _.template('\
+			<% bookmarks.each(function(bookmark) { %>\
+				<li><a href="#"><%= bookmark.get("url") %></a></li>\
+			<% }) %>\
+		'),
+
+		initialize: function() {
+			this.listenTo(this.collection, 'update', this.render);
+		},
+
+		render: function() {
+			this.$el.html(this.template({ bookmarks: this.collection }));
+			return this;
+		}
+	})
+
+
+	var homeView = new HomeView();
+	$('#content').html(homeView.render().el);
+
+// })
+
 	
