77cf6d603aaa1d884fa87f91caad8d69e1579bde
Traci Elswick <telswick@austin.rr.com>
2016-04-05 19:04:02 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,17 +12,31 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+    <style>
+        body {
+            font-size: 14px;
+        }
 
+        .container {
+            width: 100%;
+        }
+
+        #all-subbreddits {
+            height: 600px;
+            overflow: scroll;
+        }
+
+        #posts {
+            height: 300px;
+            overflow: scroll;
+        }
+    </style>
     
 </head>
 <body>
     <div id="content"></div>
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- JavaScripts (moving all but bundle.js) -->
+    <script src="{{ asset('js/bundle.js') }}"></script>
 
 </body>
 </html>
-
